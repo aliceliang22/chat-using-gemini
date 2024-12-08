8302007a96396f267e60e5964ea3cf6b796a0c72
Alice Liang <aliceliang8888@gmail.com>
2024-12-07 19:50:09 -0600

    add status 200

--- src/app.py
+++ src/app.py
@@ -10,10 +10,10 @@ def main():
     <main style = " text-align: center; ">
         <h2> Chat With Private Data Using Gemini from Google </h2>
         <h4> Developed by Alice Liang </h4>
-
+        <p> Please enter your text in the following text box, then hit the submit button </p>
         <form action="/echo_user_input" method="POST">
             <input name="user_input">
-            <input type="submit" value="Enter">
+            <input type="submit" value="Sobmit">
         </form>
     </main>
     '''
@@ -21,7 +21,8 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    response = "You entered: " + input_text + "<br> Status: 200 OK"
+    return response, 200
 
 if __name__ == '__main__':
 	    app.run(debug=True)
