650b1606ff015adb5fb164b5f79892d21580cbd5
Alice Liang <aliceliang8888@gmail.com>
2024-12-07 20:05:14 -0600

    replace text to name

--- src/app.py
+++ src/app.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return '''
-    <main style = " text-align: center; ">
+    <main style = "text-align: center; ">
         <h2> Chat With Private Data Using Gemini from Google </h2>
         <h4> Developed by Alice Liang </h4>
-        <p> Please enter your text in the following text box, then hit the submit button </p>
+        <p> Please enter your name in the following text box, then hit the submit button </p>
         <form action="/echo_user_input" method="POST">
             <input name="user_input">
-            <input type="submit" value="Sobmit">
+            <input type="submit" value="Submit">
         </form>
     </main>
     '''
@@ -21,7 +21,7 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    response = "You entered: " + input_text + "<br> Status: 200 OK"
+    response = '<h4 style = "text-align: center;"> Welcome ' + input_text + '! <br> Status: 200 OK </h4>'
     return response, 200
 
 if __name__ == '__main__':
