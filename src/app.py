73b8cdb9486f1f43aad2f0791cfdbd80ffd0ef0c
Alice Liang <aliceliang8888@gmail.com>
2024-12-10 00:53:39 -0600

    change input type to files

--- src/app.py
+++ src/app.py
@@ -8,11 +8,10 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    response = '<h4 style = "text-align: center;"> Welcome ' + input_text + '! <br> Status: 200 OK </h4>'
-    return response, 200
+@app.route('/upload', methods=['POST'])
+def upload():
+    files = request.files.getlist("documents")
+    return "Documents Uploaded Successfully. <br> Status: 200 OK"
 
 if __name__ == '__main__':
 	    app.run(debug=True)
